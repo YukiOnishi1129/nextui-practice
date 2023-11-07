8edcbe8f0aeea329d2e91cb0bcf9b1ca2a17e43f
yukionishi1129 <yuki.onishi1129@gmail.com>
2023-11-08 01:44:10 +0900

    profile matcher

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,6 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   // ログインページ（/login）とトップページ(/)以外を対象にする
-  matcher: ["/((?!login|).*)"],
+  // matcher: ["/((?!login|).*)"],
+  matcher: ["/profile"],
 };
