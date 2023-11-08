588d534d93d76af1ff56c190648aacd45f54d19d
yukionishi1129 <yuki.onishi1129@gmail.com>
2023-11-09 01:34:38 +0900

    google and github ui button

--- app/(admin)/login/page.tsx
+++ app/(admin)/login/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { Button } from "@nextui-org/react";
+import { FcGoogle } from "react-icons/fc";
+import { GrGithub } from "react-icons/gr";
 
 export default function Login() {
   return (
-    <Button onClick={() => signIn("google", { callbackUrl: "/" })}>
-      Login with Google
-    </Button>
+    <>
+      <Button
+        startContent={<FcGoogle />}
+        variant="bordered"
+        onClick={() => signIn("google", { callbackUrl: "/" })}
+      >
+        Login with Google
+      </Button>
+      <Button
+        startContent={<GrGithub />}
+        variant="bordered"
+        onClick={() => signIn("github", { callbackUrl: "/" })}
+      >
+        Login with Github
+      </Button>
+    </>
   );
 }
